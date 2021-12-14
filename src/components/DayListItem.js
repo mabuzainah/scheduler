ea5e4ae38bee02dc4a3583e8d5ff38a2d8fcf0cc
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-14 12:07:40 -0500

    Update DayListItem.js to utilize ClassName JS library to render the different styles based off the state of the button.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,19 @@
 import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
-    </li>
-  );
+    const dayListItemClass = classnames("day-list__item", {
+        "day-list__item--selected": props.selected,
+        "day-list__item--full": props.full
+     });
+    return (
+        <li 
+            onClick={() => props.setDay(props.name)}
+            className= {dayListItemClass}
+        >
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{props.spots}</h3>
+        </li>
+    );
 }
