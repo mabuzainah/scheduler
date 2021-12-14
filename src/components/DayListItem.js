6d42dab9ca14e0ca511984d8b4ac27eef79e460f
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-14 12:35:22 -0500

    Update DayListItem.js to include a function to allow JIT testing to work
    Created formatSpots function that formats the spots available to match what is passed to DayListItem for spots available.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,13 +7,25 @@ export default function DayListItem(props) {
         "day-list__item--selected": props.selected,
         "day-list__item--full": props.full
      });
+    
+    const formatSpots = function (spotsAvailable) {
+        if (!spotsAvailable) {
+            return `no spots remaining`;
+        } else if (spotsAvailable === 1) {
+            return `${spotsAvailable} spot remaining`;
+        }
+        return `${spotsAvailable} spots remaining`;
+    };
+
+    const availableSpots = formatSpots(props.spots);
+
     return (
         <li 
             onClick={() => props.setDay(props.name)}
             className= {dayListItemClass}
         >
         <h2 className="text--regular">{props.name}</h2> 
-        <h3 className="text--light">{props.spots}</h3>
+        <h3 className="text--light">{availableSpots}</h3>
         </li>
     );
 }
