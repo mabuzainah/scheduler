a91c9d98c8357b19e0d93337ba0fee33d40df99b
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-14 12:41:23 -0500

    Update index.js
    Updating index.js to include fake dummy data for days and the stories for Storybook to test out DayList component.

--- stories/index.js
+++ stories/index.js
@@ -39,3 +39,33 @@ storiesOf("DayListItem", module)
     // ^ action() allows us to create a callback that appears in 
     //   the actions panel when clicked
   ));
+
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+  
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
