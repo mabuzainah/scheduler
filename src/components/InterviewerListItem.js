58706ccbe450109004c4feaf5a7178293e903bf6
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-15 11:46:16 -0500

    Update InterviewListItem component to include 1 user for now
    Replaced the fragment with some hardcoded text and values for the user Sylvia Palmer who is going to be one of the interviewers.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,7 +3,20 @@ import classnames from "classnames";
 import "components/InterviewListItem.scss";
 
 export default function InterviewListItem(props) {
+    // this component will take in the following as props:
+        // id:number - the id of the interviewer
+        // name:string - the name of the interviewer
+        // avatar:url - a URL to an image of the interviewer
+        // selected:boolean - to determine if an interview is selected or not
+        // setInterviewer:function - sets the interviewer upon selection
     return (
-        <></>
+        <li className="interviewers__item">
+            <img
+            className="interviewers__item-image" 
+            src="https://i.imgur.com/LpaY82x.png" 
+            alt="Sylvia Palmer" 
+            />
+            Sylvia Palmer
+        </li>
     );
 }
