0315ea486fb68ba62f864379466471c7a07a6385
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-15 16:29:21 -0500

    Update InterviewerListitem component to use the props instead of hardcoded text
    For now commented out the hard-coded text. Instead now using props.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,24 @@
 import React from "react";
 import classnames from "classnames";
-import "components/InterviewListItem.scss";
+import "components/InterviewerListItem.scss";
 
-export default function InterviewListItem(props) {
+export default function InterviewerListItem(props) {
     // this component will take in the following as props:
         // id:number - the id of the interviewer
         // name:string - the name of the interviewer
         // avatar:url - a URL to an image of the interviewer
         // selected:boolean - to determine if an interview is selected or not
         // setInterviewer:function - sets the interviewer upon selection
+
+    const interviewerListItemClass = classnames("interviewers__item", {
+        "interviewers__item--selected": props.selected
+    });
+
     return (
-        <li className="interviewers__item">
+        <li 
+        className={interviewerListItemClass}
+        onClick={() => props.setInterviewer(props.setInterviewer)}
+        >
             <img
             className="interviewers__item-image" 
             src="https://i.imgur.com/LpaY82x.png" 
