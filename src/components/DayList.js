92a5f7e4aa8437889d98269fbc5845a436be0479
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-14 21:50:21 -0500

    Update DayLIst to render DayListItem component based off an array
    Using the array.map() function to render component DayListItem with the list of days array from Index.js file

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,24 @@
 import React from "react";
+import DayListItem from "./DayListItem";
+
 
 export default function DayList(props) {
+    
+    const dayList = props.days.map(dayz => {
+        return (<DayListItem 
+                key= {dayz.id}
+                name={dayz.name} 
+                spots={dayz.spots}
+                selected= {dayz.name === props.day}
+                setDay= {props.setDay}
+                > 
+                    {dayz} 
+                </DayListItem>);
+    });
+
     return (
-        <ul></ul>
+        <ul>
+            {dayList}
+        </ul>
     );
 }
