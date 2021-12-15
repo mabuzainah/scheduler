0bdf7769d7ab94536525777d7e245eeeddb17947
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-15 17:46:46 -0500

    Update InterviewerList component to render a list of interviews passed to it.
    Using the array.map() function to create an element of InterviewerListItem for each interviewer, passing each necessary prop to create the InterviewerListItem for each individual interviewer.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,34 @@
 import React from "react";
 import classnames from "classnames";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerListItem(props) {
+export default function InterviewerList(props) {
     // this component will take in the following as props:
         // interviewers:array - an array of objects containing the information of
         // each interviewer
         // interviewer:number - the id of an interviewer
         // setInterviewer:function - a function that accepts an interviewer id
 
+    const interviewerList = props.interviewers.map(interviewer => {
+        return (<InterviewerListItem
+                key= {interviewer.id}
+                interviewer={interviewer.interviewer} 
+                setInterviewer= {props.setInterviewer}
+                name= {interviewer.name}
+                avatar= {interviewer.avatar}
+                selected= {interviewer.selected === props.selected}
+                > 
+                    {interviewer} 
+                </InterviewerListItem>);
+    });
+
     return (
         <section className="interviewers">
             <h4 className="interviewers__header text--light">Interviewer</h4>
-            <ul className="interviewers__list"></ul>
+            <ul className="interviewers__list">
+                {interviewerList}
+            </ul>
         </section>
     );
 }
