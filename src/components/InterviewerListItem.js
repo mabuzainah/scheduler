29804e0887854b16a00996b84c65445cb326da0d
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-15 11:42:53 -0500

    Update InterviewListItem component to have bare minimum code for a component in react
    Added bare minimum code to allow this component file to render - currently renders a fragment.
    
    Added line 3 about importing InterviewListItem.scss file that will contain styling. -- file yet to be created.

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classnames from "classnames";
+import "components/InterviewListItem.scss";
+
+export default function InterviewListItem(props) {
+    return (
+        <></>
+    );
+}
