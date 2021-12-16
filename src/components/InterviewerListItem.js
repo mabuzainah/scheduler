d223efaf441cd8d12182a0963ce53ff5d8e05bbe
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-15 19:34:24 -0500

    Update InterviewListItem component to properly reflect status when user is not selected
    Added a line of code to display the user's name ONLY when selected and adjusted the onClick function to reflect the interviewer's name when clicked on.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -13,18 +13,23 @@ export default function InterviewerListItem(props) {
     const interviewerListItemClass = classnames("interviewers__item", {
         "interviewers__item--selected": props.selected
     });
+    // NaN, null, "", 0, underfined, false    are FALSY values.  
+    // modified line 33 to be {props.selected && props.name} instead of {props.name}. The reason
+    // why we put this in there, is to allow the name only to display if interviewer was selected. 
+    // then we put props.selected ahead of props.name, to get the name to return. 
+    // So when selected === false, then the && operator will return false and not return anything.
 
     return (
         <li 
         className={interviewerListItemClass}
-        onClick={()=> props.setInterviewer}
+        onClick= {() => props.setInterviewer(props.name)}
         >
             <img
             className="interviewers__item-image" 
             src= {props.avatar}
             alt= {props.name}
             />
-            {props.name}
+            {props.selected && props.name}
         </li>
     );
 }
