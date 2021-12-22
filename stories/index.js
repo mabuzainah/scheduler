9b95de3ca9187271b4d957cd722487caac77254c
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-21 21:13:51 -0500

    Update index.js to include story for the Confirm component.
    Currently incomplete and to be added to.

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,5 +153,10 @@ storiesOf("Appointment", module)
       interviewer={interviewer}
       onClick={action("onEdit")}
       onClick={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm 
+
     />
   ));
