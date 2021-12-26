b12188cdfa05adadc211d25c0805846c8ad073d9
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-26 14:25:08 -0500

    Update Application.js to display the appointments available based off fixed set of data for available appointments
    Iterating over the array using a map to populate the data from the appointments array.

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,54 @@ import React , {useState} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Alex Black",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Kookoo Baba",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
@@ -22,6 +70,16 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const appointmentList = appointments.map(appointment => {
+    return (<Appointment
+            key= {appointment.id}
+            id={appointment.id}
+            time= {appointment.time}
+            interview= {appointment.interview}
+            > 
+            </Appointment>);
+  });
 
   return (
     <main className="layout">
@@ -46,7 +104,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
