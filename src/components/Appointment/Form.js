bef3a0c53014296120075e5cfd9a0e4b86c9f82b
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-26 13:31:24 -0500

    Update Form component of the Appointment component to include states
    Adding State to the Appointment Form and updating the code to better utilize the states and setting them.
    
    Using useState hook in react to keep track of two pieces of information the name from our input and the selected interviewer.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from 'react'
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
     
-    const currentNameOfStudent = props.name;
+    const [currentNameOfStudent, setNameOfStudent] = useState(props.name || "");
+    const [currentInterviewer, setInterviewer] = useState(props.value || null);
     
+    const reset = function () {
+        setNameOfStudent("");
+        setInterviewer(null);
+    };
+
+    const cancel = function () {
+        reset();
+        props.onCancel();
+    };
+
     return (
         <main className="appointment__card appointment__card--create">
         <section className="appointment__card-left">
             <form autoComplete="off">
             <input
                 className="appointment__create-input text--semi-bold"
-                name={props.name}
+                name={currentNameOfStudent}
+                onChange={(event) => setNameOfStudent(event.target.value)} 
+                value = {currentNameOfStudent}
                 type="text"
                 placeholder={currentNameOfStudent? currentNameOfStudent: "Please enter your name"}
                 /*
@@ -20,12 +33,12 @@ export default function Form(props) {
                 */
             />
             </form>
-            <InterviewerList interviewers={props.interviewers} value={props.value} onChange={props.value}/>
+            <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)}/>
         </section>
         <section className="appointment__card-right">
             <section className="appointment__actions">
-            <Button danger onCancel={props.onCancel}>Cancel</Button>
-            <Button confirm onSave={props.onSave}>Save</Button>
+                <Button danger onClick={cancel}>Cancel</Button>
+                <Button confirm onSubmit={event => event.preventDefault()} onClick={event => props.onSave(currentNameOfStudent, currentInterviewer)}>Save</Button>
             </section>
         </section>
         </main>
