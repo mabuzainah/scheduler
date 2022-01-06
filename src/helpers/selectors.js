26804a100db0e4cdb03caf7515e1cf1df7f78810
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2022-01-06 13:00:18 -0500

    Update selector file to include a getInterviewersForDay function
    A selector function to obtain the interviewers for a day. Based off implementation of getAppointmentsForDay function in the same file.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,7 @@
+/*
+  A selector function to that returns the Appointments for a Day
+*/
+
 export function getAppointmentsForDay(state, day) {
     //... returns an array of appointments for that day
     
@@ -16,6 +20,11 @@ export function getAppointmentsForDay(state, day) {
 
 }
 
+/*
+    A helper function that returns an object containing the student and the 
+    interviewer based off an interview passed and the state passed to the function.
+*/
+
 export function getInterview(state,interview) {
     //checking to see if the interview being passed is null, if null then return nothing.
     if (!interview) {
@@ -27,4 +36,28 @@ export function getInterview(state,interview) {
         student: interview.student,
         interviewer: state.interviewers[interview.interviewer]
     }
+}
+
+/*
+    A selector function to obtain the Interviewers for a Day, contains
+    slight modification of the variables from getAppointmentDays selector
+    function above in the file. 
+*/
+
+export function getInterviewersForDay(state, day) {
+    //... returns an array of interviewers for that day
+    
+    //creating an empty array that will be used to append the interviewers for a given day to it
+    //based of the state passed.
+    let interviewerArray = [];
+    
+    for (const dayFromState of state.days){
+        if (dayFromState.name === day) {
+            dayFromState.interviewers.forEach(interviewerId => interviewerArray.push(interviewerId)); 
+        }
+    };
+   
+    const matchingInterviewers = interviewerArray.map( id => state.interviewers[id]);
+    return matchingInterviewers;
+
 }
