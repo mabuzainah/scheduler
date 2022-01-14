773460d5260dc3a4354f83eb17ecce9e8ce3d584
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2022-01-13 20:27:19 -0500

    Update useVisualMode to use useHistory - per clarification from instructor in lecture
    Updated the code to have useVisualMode use useHistory and pass the tests for useVisualMode.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,36 +1,42 @@
 import { useState } from "react"
 
-export function useVisualMode(intialMode) {
-    const [mode, setMode] = useState(intialMode);
+export function useVisualMode(initial) {
+    
+    const [mode, setMode] = useState(initial);
     // keeping track of the actions on the UI such that we are initially 
     // storing the initial mode in an array. Interaction of the history
     // will be done through the Transition and Back functions/actions.
 
-    const [history, setHistory] = useState([intialMode]); // This line is new!
+    const [history, setHistory] = useState([initial]); // This line is new!
     
-    function transition (mode, replace = false) {
+    function transition (newMode, replace = false) {
         if (replace) {
-            setMode(mode);
-            
+            setMode(newMode);
+            setHistory((prev) => {
+                prev.pop();
+                prev.push(newMode)
+                return prev;
+            });
+            console.log("transition from " , mode, "to ", newMode);
         } else {
-            setMode(mode);
+            setMode(newMode);
             //using the array method of push to append to the end of 
             //the array.
-            history.push(mode);
+            setHistory((prev) => {
+                prev.push(newMode);
+                return prev;
+            });
+            console.log("transition from " , mode, "to ", newMode);
         }
     }
 
     function back () {
-        if (history.length >= 1) {
-            //using the array method of Pop to remove the last element 
-            //from the array
-            history.pop();
-            // setting the mode to the last element of the array; accessed 
-            // based off array length.
-            setMode(history[history.length - 1]);
-        } if (history.length < 1) {
-            setMode(mode);
-        }
+        const newHistory = [...history];
+        newHistory.pop();
+        setHistory((prev) => newHistory);
+        if (history.length > 1) {
+            setMode(newHistory[newHistory.length - 1]);
+        }    
     }
 
     return {mode, transition, back};
