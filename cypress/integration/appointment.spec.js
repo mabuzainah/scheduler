c9d270c6cd124ad92117abbe0785744b476fed90
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2022-01-22 15:11:05 -0500

    Update Appointments testing with Cypress to include Editing an interview
    Added implementation of Edit functionality using Cypress.

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -55,7 +55,14 @@ Clicks the save button
 Sees the edit to the appointment
 
 */
-   
+    it("should edit an interview", () =>{
+        cy.get("[alt=Edit]").first().click({force: true});
+        cy.get('[alt="Tori Malcolm"]').click();
+        cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+        cy.contains("Save").click();
+        cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+        cy.contains(".appointment__card--show", "Tori Malcolm");
+    });
 
 /* Canceling
 We can also perform a test to cancel an existing interview. It is for this reason that we need to reset the database after each test. If one test cancels and interview and the next test expects that interview to exist, then our tests can break for reasons unrelated to our code quality.
