43c073813d3935160c74834c7eef184150bf0f22
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2022-01-22 15:23:55 -0500

    Implement testing with Cypress for deleting an appointment
    utilizing the properly of .should('not.exist') of cypress we verify that an element or indicator is no longer present.

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -31,15 +31,10 @@ Sees the booked appointment
 */
 
     it("should book an interview", () => {
-        cy.get("[alt=Add]")
-        .first()
-        .click();
-    
+        cy.get("[alt=Add]").first().click();
         cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
         cy.get('[alt="Sylvia Palmer"]').click();
-    
         cy.contains("Save").click();
-    
         cy.contains(".appointment__card--show", "Lydia Miller-Jones");
         cy.contains(".appointment__card--show", "Sylvia Palmer");
     });
@@ -73,6 +68,12 @@ Clicks the confirm button
 Sees that the appointment slot is empty
 
 */
-
+    it("should cancel an interview", () =>{
+        cy.get("[alt=Delete]").first().click({force: true});
+        cy.contains('Confirm').click();
+        cy.contains("Deleting");
+        cy.contains("Deleting").should('not.exist');
+        cy.contains(".appointment__card--show", "Archie Cohen").should('not.exist');
+    });
 
 });
